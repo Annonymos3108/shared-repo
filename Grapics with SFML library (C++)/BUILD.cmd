467a61af688f53d34c53edc4fa99bf5725304656
Arthur Nersisyan <24681914+Annonymos3108@users.noreply.github.com>
2022-02-15 15:58:57 +0300

    Delete Grapics with SFML library (C++) directory

--- Grapics with SFML library (C++)/BUILD.cmd
+++
@@ -1,6 +0,0 @@
-xcopy /s /Y "C:\Program Files\SFML-2.5.1\bin\sfml-window-2.dll" .\bin\
-xcopy /s /Y "C:\Program Files\SFML-2.5.1\bin\sfml-system-2.dll" .\bin\
-xcopy /s /Y "C:\Program Files\SFML-2.5.1\bin\sfml-graphics-2.dll" .\bin\
-mkdir .\bin\Polyhedron_Lightning
-mingw32-make
-pause
